0c151d00876e78d16ce5a0899054b33b306dcbf2
Jeff Zuccaro <drumsetz@hotmail.com>
2015-03-28 14:18:28 -0400

    Update schema.rb
    Commits the new schema.rb file with sub_sprokets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325195940) do
+ActiveRecord::Schema.define(version: 20150326201725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20150325195940) do
     t.string  "name"
     t.integer "rank"
     t.integer "project_id"
+  end
+
+  create_table "sub_sprokets", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "rank"
+    t.integer  "sproket_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
