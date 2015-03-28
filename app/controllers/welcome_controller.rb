d52e714d5efd929d64d68673a76f5ac57dd7eb68
jonBarcus <barcus.j@gmail.com>
2015-03-28 14:29:07 -0400

    Got basic Sproket/Project - Subsproket relationship working.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
   def index
     @project = Project.take
     @available_sprokets = Sproket.where(project_id: nil)
+    binding.pry
   end
 
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
 
   has_many :sprokets
-
+  has_many :sub_sprokets, through: :sprokets
 end
